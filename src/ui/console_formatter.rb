d217da8bcabd81ef048cb5d6e339ffbb5157771f
lukblan <luk.blanko@gmail.com>
2023-12-19 11:47:56 -0300

    Add user interface basic interactions

--- src/ui/console_formatter.rb
+++ src/ui/console_formatter.rb
@@ -2,4 +2,33 @@ class ConsoleFormatter
   def initialize(line_length)
     @line_length = line_length
   end
+
+  def display_message_between_dashes(message)
+    line = self.get_line
+    message_in_middle = get_text_in_middle(message)
+
+    puts(line)
+    puts(message_in_middle)
+    puts(line)
+  end
+
+  def print_indent_message(message)
+    puts("    #{message}")
+  end
+
+  def get_line
+    line = ""
+    @line_length.times { line += "-" }
+    line
+  end
+
+  def get_text_in_middle(message)
+    text = ""
+    middle_point = @line_length / 2
+    message_offset_to_middle = message.length / 2
+    white_spaces_amount = middle_point - message_offset_to_middle
+
+    white_spaces_amount.times { text += " "}
+    text + message
+  end
 end
--- src/ui/console_interface.rb
+++ src/ui/console_interface.rb
@@ -1,9 +1,30 @@
 class ConsoleInterface
-  def initialize(console_formatter)
+  def initialize(console_formatter, file_reader)
     @console_formatter = console_formatter
+    @file_reader = file_reader
   end
 
   def init
-    puts("Hi there!")
+    self.show_welcome_message("Welcome to Sudoku")
+    self.show_available_files
+    puts
+    file_name = get_file_from_user_input
+  end
+
+  def show_welcome_message(message)
+    @console_formatter.display_message_between_dashes(message)
+  end
+
+  def show_available_files
+    files = @file_reader.get_available_files
+    puts("Available Files: ")
+    files.each do |file|
+      @console_formatter.print_indent_message(file)
+    end
+  end
+
+  def get_file_from_user_input
+    print("Select a game file: ")
+    gets.chomp
   end
 end
