8055d11f7ab79aa04750034899df7e3c26b165d4
lukblan <luk.blanko@gmail.com>
2023-12-19 11:46:32 -0300

    Add dir path to file_reader
    FileReader now get all available files

--- src/services/file_reader.rb
+++ src/services/file_reader.rb
@@ -1,7 +1,15 @@
 class FileReader
+  def initialize(path_to_dir)
+    @path_to_dir = path_to_dir
+  end
+
   def read_file(file_name)
     File.readlines(file_name).each do |line|
       puts(line)
     end
+  end
+
+  def get_available_files
+    Dir.entries(@path_to_dir)
   end
 end
